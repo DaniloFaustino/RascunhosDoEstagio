a815b5fb1853d75380c28b30fee558cb25db6c22
Lucas <lucasfernandes.um@gmail.com>
2018-10-29 15:51:41 -0300

    IT SEEMS LIKE THE END, BUT IT'S ONLY THE BEGGINING! = Faustino, Danilo Pocket can try to kill us, but we r the resistance!

---
+++ InterfacesTestes/InterfacesTestes/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace InterfacesTestes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtMatricula = new System.Windows.Forms.TextBox();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtMatricula
+            // 
+            this.TxtMatricula.Location = new System.Drawing.Point(169, 118);
+            this.TxtMatricula.Name = "TxtMatricula";
+            this.TxtMatricula.Size = new System.Drawing.Size(100, 20);
+            this.TxtMatricula.TabIndex = 0;
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Location = new System.Drawing.Point(169, 158);
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.PasswordChar = '*';
+            this.TxtSenha.Size = new System.Drawing.Size(100, 20);
+            this.TxtSenha.TabIndex = 1;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(169, 185);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(100, 23);
+            this.BtnLogin.TabIndex = 2;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 273);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.TxtMatricula);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtMatricula;
+        private System.Windows.Forms.TextBox TxtSenha;
+        private System.Windows.Forms.Button BtnLogin;
+    }
+}
+
---
+++ InterfacesTestes/InterfacesTestes/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace InterfacesTestes
+{
+    public partial class Form1 : Form
+    {
+
+        string usuario= "programador";
+        string senha = "Cp2";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LabCad_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Cad cadastro = new Cad();
+            cadastro.ShowDialog();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection conex = new SqlConnection("Data Source = localhost; Initial Catalog = BancoMec; Integrated Security = SSPI;");
+            SqlCommand comd = new SqlCommand();
+            comd.Connection = conex;
+
+            comd.CommandText = "SELECT Matricula FROM Adm WHERE MATRICULA = @Matricula AND SENHA = @Senha";
+            comd.Parameters.AddWithValue("Matricula", TxtMatricula.Text);
+            comd.Parameters.AddWithValue("Senha", TxtSenha.Text);
+
+            comd.Connection.Open();
+            SqlDataReader reader = comd.ExecuteReader();
+            int id = 0;
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+            }
+            reader.Close();
+            comd.Connection.Close();
+
+            if (id == 0)
+            {
+                MessageBox.Show("Informações Inválidas");
+            }
+            else
+            {
+                Cad cadastro = new Cad();
+                cadastro.Show();
+            }
+
+            /* 
+             if (TxtMatricula.Text == usuario && TxtSenha.Text == senha)
+            {
+               Cad cad = new Cad();
+                cad.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuario invalido");
+            } 
+            */
+        }
+    }
+}
